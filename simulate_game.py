7c5d832572a83220bb190f14d7e48c39ecd26118
Scott-Huston <scottphuston@gmail.com>
2020-05-26 14:19:13 -0700

    added fn to get defensive scores

--- simulate_game.py
+++ simulate_game.py
@@ -2,17 +2,17 @@ from random import randint
 import pandas as pd
 
 ### example lineups
-# home_lineup = [[2911, 'LF/RF']
-#                 [454, 'C']
-#                 [694, 'LF-RF']
-#                 [3676, 'DH']
-#                 [288, '2B']
-#                 [2656, '3B']
-#                 [200, '1B']
-#                 [1103, 'SS']
-#                 [3806, 'CF']]
+# home_lineup = [[2912, 'LF-RF'],
+#                 [455, 'C'],
+#                 [695, 'LF-RF'],
+#                 [3677, 'DH'],
+#                 [289, '2B'],
+#                 [2657, '3B'],
+#                 [201, '1B'],
+#                 [1104, 'SS'],
+#                 [3807, 'CF']]
 
-class game:
+class Game:
     def __init__(self, home_lineup, home_pitching_dict, away_lineup, away_pitching_dict):
         # TODO add strategy settings for stealing/bunting and maybe icons/random bonuses
         # TODO implement batter classes to keep track of stats and icon/bonus usage
@@ -29,58 +29,60 @@ class game:
         self.home_batter = 1
         self.away_batter = 1
         self.players = pd.read_csv('Showdown_Charts.csv')
-        
-        self.calculate_def_scores()
+        self.home_defense = self.get_def_scores(self.home_lineup)
+        self.away_defense = self.get_def_scores(self.away_lineup)
 
-    def calculate_def_scores(self):
+    def get_def_scores(self, lineup):
         IF_positions = ['1B', '2B', 'SS', '3B']
         OF_positions = ['LF-RF', 'CF']
 
-        self.home_defense = {'C':0,
-                            'IF':0,
-                            'OF':0}
+        defense = {'C':0,
+                    'IF':0,
+                    'OF':0}
 
-        for entry in self.home_lineup:
-            player = self.players.loc[self.players['ID'] == entry[0]]
+        for entry in lineup:
+            player = self.players.loc[self.players['ID'] == entry[0]].iloc[0]
 
             ### Getting catcher score ###
             if entry[1] == 'C':
                 # check Position1 (checking for 'C+' so CF isn't counted)
-                if 'C+' in player.Position1[0]:
-                    fielding_score = player.Position1[0].split['+'][-1]
-                    self.home_defense['C'] = fielding_score
+                if 'C+' in player.Position1:
+                    fielding_score = int(player.Position1.split('+')[-1])
+                    defense['C'] = fielding_score
                 # check Position2
-                elif 'C+' in player.Position2[0]:
-                    fielding_score = player.Position2[0].split['+'][-1]
-                    self.home_defense['C'] = fielding_score
+                elif 'C+' in player.Position2:
+                    fielding_score = int(player.Position2.split('+')[-1])
+                    defense['C'] = fielding_score
                 else:
                     raise ValueError(f'player {entry[0]} in invalid position')
             
             ### Getting IF score ###
             if entry[1] in IF_positions:
                 # check Position1 
-                if entry[1] in player.Position1[0] or 'IF' in player.Position1[0]:
-                    fielding_score = player.Position1[0].split['+'][-1]
-                    self.home_defense['IF'] += fielding_score
+                if entry[1] in player.Position1 or 'IF' in player.Position1:
+                    fielding_score = int(player.Position1.split('+')[-1])
+                    defense['IF'] += fielding_score
                 # check Position2
-                elif entry[1] in player.Position2[0] or 'IF' in player.Position1[0]:
-                    fielding_score = player.Position2[0].split['+'][-1]
-                    self.home_defense['IF'] += fielding_score
+                elif entry[1] in player.Position2 or 'IF' in player.Position1:
+                    fielding_score = int(player.Position2.split('+')[-1])
+                    defense['IF'] += fielding_score
                 else:
                     raise ValueError(f'player {entry[0]} in invalid position')
             
             ### Getting OF score ###
             if entry[1] in OF_positions:
                 # check Position1 
-                if entry[1] in player.Position1[0] or 'OF' in player.Position1[0]:
-                    fielding_score = player.Position1[0].split['+'][-1]
-                    self.home_defense['OF'] += fielding_score
+                if entry[1] in player.Position1 or 'OF' in player.Position1:
+                    fielding_score = int(player.Position1.split('+')[-1])
+                    defense['OF'] += fielding_score
                 # check Position2
-                elif entry[1] in player.Position2[0] or 'OF' in player.Position1[0]:
-                    fielding_score = player.Position2[0].split['+'][-1]
-                    self.home_defense['OF'] += fielding_score
+                elif entry[1] in player.Position2 or 'OF' in player.Position1:
+                    fielding_score = int(player.Position2.split('+')[-1])
+                    defense['OF'] += fielding_score
                 else:
                     raise ValueError(f'player {entry[0]} in invalid position')
+            
+        return defense
     
     def lookup_result(self,  swing, advantage_player_id):
         result_cols = ['PU', 'SO', 'GB', 'FB', 'BB', 'Single', 'SinglePlus', 'Double', 'Triple', 'HR']
